e755a7ecee79ec23325d0e112062374aea4c3ffc
Balázs Marcell <KoegLike@proton.me>
2025-04-14 22:39:00 +0300

    Update wake_on_lan_server.py

--- wol-script/wake_on_lan_server.py
+++ wol-script/wake_on_lan_server.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 # Replace with the MAC address of the target PC
 TARGET_MAC_ADDRESS = "00:15:5D:05:43:00"
+TARGET_IP_ADDRESS="192.168.5.213"
 
 @app.route('/start', methods=['GET'])
 def wake_pc():
     try:
         # Send the WOL magic packet
-        send_magic_packet(TARGET_MAC_ADDRESS)
+        send_magic_packet(TARGET_MAC_ADDRESS, TARGET_IP_ADDRESS)
         return jsonify({"status": "success", "message": "Magic packet sent to wake the PC."}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
